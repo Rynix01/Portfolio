8e0de111c0bb202315c9b2ea9b3a7b77e140a259
Murat Bulut <murobey729@gmail.com>
2023-03-15 19:32:23 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,19 +11,6 @@ menuToggle.onclick = function () {
   header.classList.toggle("active");
 };
 
-let list = document.querySelectorAll(".nav li");
-function active() {
-  list.forEach((i) => i.classList.remove("active"));
-  this.classList.add("active");
-}
-list.forEach((i) => i.addEventListener("click", active));
-
-let menuToggle = document.querySelector(".menuToggle");
-let header = document.querySelector("header");
-menuToggle.onclick = function () {
-  header.classList.toggle("active");
-};
-
 var Text = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
