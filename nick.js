51622d88e9b5c73681e7aeee8f0f312aa6f5609f
Murat Bulut <murobey729@gmail.com>
2023-05-09 00:13:05 +0300

    Delete nick.js

--- nick.js
+++
@@ -1,9 +0,0 @@
-const t = document.querySelector("title"),
-  w = "Rynix",
-  e = "El Chavo";
-let c = 0;
-setInterval(() => {
-  c = c < w.length ? c + 1 : 0;
-  t.textContent = w.slice(0, c);
-  if (c === w.length) setTimeout(() => (t.textContent += `${e}`), 500);
-}, 450);
